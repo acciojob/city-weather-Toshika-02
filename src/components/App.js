90a78a5b8f709a0494db87d4cb1c07b1dd090eda
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-04-23 21:40:58 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState(null);
+  function getData() {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=7d8db3a49de7c4affd734cb6dfcab991`
+    )
+      .then((data) => data.json())
+      .then((res) => {
+        setData(res);
+        if (res.cod == 200) setQuery("");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  console.log(data);
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <input
+        type="text"
+        className="search"
+        onChange={(e) => {
+          getData(e.target.value);
+          setQuery(e.target.value);
+        }}
+        value={query}
+      />
+      <div className="weather" style={{height:"10rem"}}>{data?.main && <h1>{data.main.temp} F</h1>}</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
